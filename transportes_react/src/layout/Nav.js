36a034eadd6295ccef62c2fb18dfb0677c9f1c43
Romina Mauro <rominaraymonda@gmail.com>
2023-06-14 12:06:40 -0300

    clase 27

--- transportes_react/src/layout/Nav.js
+++ transportes_react/src/layout/Nav.js
@@ -1,15 +1,15 @@
-import { Link } from 'react-router-dom';
-
+//import { Link } from 'react-router-dom';
+import {NavLink} from "react-router-dom";
 
 const Nav = (props) =>{
     return(
         <nav>
         <ul className="holder">
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/nosotros">Nosotros</Link></li>
-            <li><Link to="/servicios">Servicios</Link></li>
-            <li><Link to="/galeria">Galeria</Link></li>
-            <li><Link to="/contactos">Contactos</Link></li>
+            <li><NavLink to="/" className={({ isActive}) => isActive ? "activo" :undefined}>Home</NavLink></li>
+            <li><NavLink to="/nosotros" className={({ isActive}) => isActive ? "activo" :undefined}>Nosotros</NavLink></li>
+            <li><NavLink to="/servicios" className={({ isActive}) => isActive ? "activo" :undefined}>Servicios</NavLink></li>
+            <li><NavLink to="/galeria" className={({ isActive}) => isActive ? "activo" :undefined}>Galeria</NavLink></li>
+            <li><NavLink to="/contactos" className={({ isActive}) => isActive ? "activo" :undefined}>Contactos</NavLink></li>
         </ul>
     </nav>
     )
